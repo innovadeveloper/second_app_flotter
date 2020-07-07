1ba6dc4a6aa069ef36a6bd75bafe1a774b8b4537
Kenny Baltazar Alanoca <innovaappstar@gmail.com>
2020-07-07 15:06:39 -0500

    floating action button center added and iconbutton in actions from appbar

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,50 +1,59 @@
 import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
-
   final titleController = TextEditingController();
   final amountController = TextEditingController();
   final Function addTx;
 
   NewTransaction(this.addTx);
 
+  void submitData() {
+    // print(titleController.text);
+    // print(amountController.text);
+    if (titleController.text.isEmpty || amountController.text.isEmpty) return;
+
+    final double amount = double.parse(amountController.text);
+    if(amount <= 0)return;
+
+    this.addTx(titleController.text, amount);
+  }
+
   @override
   Widget build(BuildContext context) {
     return // inputs
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(labelText: "Title"),
-                    controller: titleController,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: "Amount"),
-                    controller: amountController,
-                  ),
-                  FlatButton(
-                    child: Text(
-                      'Add Transaction',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    textColor: Colors.purple,
-                    onPressed: () {
-                      // print(titleController.text);
-                      // print(amountController.text);
-                      if(titleController.text.isEmpty || amountController.text.isEmpty)
-                        return;
-                      this.addTx(titleController.text, double.parse(amountController.text));
-                    },
-                  )
-                ],
-              ),
+        Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: "Title"),
+              controller: titleController,
+              onSubmitted: (_) => submitData(),
             ),
-          );
+            TextField(
+              decoration: InputDecoration(labelText: "Amount"),
+              controller: amountController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => submitData(),
+            ),
+            FlatButton(
+              child: Text(
+                'Add Transaction',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              textColor: Colors.purple,
+              onPressed: () {
+                submitData();
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
