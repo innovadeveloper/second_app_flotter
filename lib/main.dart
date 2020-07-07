80fdb652a16c63a7b125801d25c0240dffc81a48
Kenny Baltazar Alanoca <innovaappstar@gmail.com>
2020-07-07 15:43:39 -0500

    theme customized : primarySwatch, accentColor..

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber
       ),
       home: MyHomePage(),
     );
