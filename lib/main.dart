e32d949b06176a0cd2662cd0fcdfd00cdfc1aa7d
Kenny Baltazar Alanoca <innovaappstar@gmail.com>
2020-07-07 11:10:13 -0500

    first commit : widget with two cardviews only

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('CHART'),
+              elevation: 2,
+            ),
+          ),
+          Card(
+            child: Text('LIST OF TX'),
+          )
+        ],
+      ),
+    );
+  }
+}
