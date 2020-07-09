ce5aaceec6f0bcdf453857cdab310f407d04500e
Kenny <developer.innovaappstar@gmail.com>
2020-07-09 12:41:18 -0500

    cupertinoButton added in ChooseDate..

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -78,19 +81,44 @@ class _NewTransactionState extends State<NewTransaction> {
                 onSubmitted: (_) => submitData(),
               ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Text(_selectedDate == null
-                      ? 'No Date Chosen!'
-                      : 'Picked Date : ${DateFormat.yMd().format(_selectedDate)}'),
-                  FlatButton(
-                    textColor: Theme.of(context).primaryColor,
-                    child: Text(
-                      'Choose Date',
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                  Expanded(
+                    child: Text(_selectedDate == null
+                        ? 'No Date Chosen!'
+                        : 'Picked Date : ${DateFormat.yMd().format(_selectedDate)}'),
+                  ),
+                  if (Platform.isIOS)
+                    CupertinoButton(
+                      child: Text(
+                        'Choose Date',
+                      ),
+                      // color: Theme.of(context).primaryColor,
+                      onPressed: _presentDatePicker,
                     ),
-                    onPressed: _presentDatePicker,
-                  )
+
+                  // Container(
+                  //   padding: EdgeInsets.all(10),
+                  //   width: MediaQuery.of(context).size.width * 0.4,
+                  //   child: FittedBox(
+                  //     child: CupertinoButton(
+                  //       child: Text(
+                  //         'Choose Date',
+                  //       ),
+                  //       // color: Theme.of(context).primaryColor,
+                  //       onPressed: _presentDatePicker,
+                  //     ),
+                  //   ),
+                  // ),
+                  if (Platform.isAndroid)
+                    FlatButton(
+                      textColor: Theme.of(context).primaryColor,
+                      child: Text(
+                        'Choose Date',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: _presentDatePicker,
+                    )
                 ],
               ),
               RaisedButton(
